477dd4a16fd1801fccc1518ccd2ec15651b8224e
somesh <someshbadade@gmail.com>
2021-10-25 13:02:06 +0530

    joining form work statred

---
+++ app/Views/employee_joining_form.php
@@ -0,0 +1,26 @@
+<?= $this->extend('layouts/main') ?>
+
+<?= $this->section('content') ?>
+<section class="abutsSection clearfix">
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+
+<div  class="col-md-6 p-0">
+    <div class="RegitserForm justify-content-center">
+        <h2>Employee Joinign Form</h2>
+
+
+    </div>
+
+</div>
+</div>
+</div>
+</section>
+
+
+<?= $this->endSection() ?>
+
+
+<?= $this->section('javascript') ?>
+
+<?= $this->endSection() ?>
